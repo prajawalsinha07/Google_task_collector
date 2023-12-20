12444d77c9b5731fe8bbc9d3fa7cb5f970492284
Harsh Jalutharia <harshjalutharia@gmail.com>
2023-12-20 18:03:05 -0500

    Modified database file to use a class. Added jsonparser to extract data from sample json file

---
+++ database/jsonParser.py
@@ -0,0 +1,29 @@
+import json
+from database import DatabaseClass
+
+f = open('jsonOutput')
+
+data = json.load(f)
+db = DatabaseClass()
+db.Connect_to_Database()
+
+for i in data["items"]:
+    taskID = i["id"]
+    taskEtag = i["etag"]
+    taskTitle = i["title"]
+    taskStatus = i["status"]
+    taskUpdated = i["updated"][:-5]
+    if "due" in i.keys():
+        taskDue = i["due"][:-5]
+    else:
+        taskDue = None
+    if "completed" in i.keys():
+        taskCompleted = i["completed"][:-5]
+    else:
+        taskCompleted = None
+    
+    db.Add_Record(taskID, taskEtag, taskTitle, taskStatus, taskUpdated, taskDue, taskCompleted)
+
+db.Get_Records_All()
+db.Close_Connection()
+f.close()
