f06668aee9ec6e01d5127261251c5e23a7e5f360
Harsh Jalutharia <harshjalutharia@gmail.com>
2023-12-20 18:53:09 -0500

    Added comments to database.py

--- database/database.py
+++ database/database.py
@@ -16,7 +16,7 @@ class DatabaseClass:
         _connection = None
         _cursor = None
 
-    
+    # Initialise variables and connect to the database
     def Connect_to_Database(self): 
         try:
             self._connection = sqlite3.connect('tasks.db')
@@ -24,11 +24,11 @@ class DatabaseClass:
         except sqlite3.Error as er:
             print('SQLite error: %s' % (' '.join(er.args)))
     
-
+    # Close connection to the database
     def Close_Connection(self):
         self._connection.close()
     
-
+    # Internal function not meant for API use. Executes sql queries
     def _execute_sql_command(self, command:str):
         try:
             self._cursor.execute(command)
@@ -37,7 +37,7 @@ class DatabaseClass:
             print('SQLite error: %s' % (' '.join(er.args)))
             print("Error Command:", command)
 
-
+    # Creates table 'Tasks'
     def Create_Table(self):
         sql_create_table_command = "CREATE TABLE Tasks("
         for column_name,column_type in self._TABLE_COLUMNS.items():
@@ -45,12 +45,12 @@ class DatabaseClass:
         sql_create_table_command = sql_create_table_command[:-2] + ");"
         self._execute_sql_command(sql_create_table_command)
 
-
+    # Deletes table 'Tasks'
     def Delete_Table(self):
         sql_drop_table_command = "DROP TABLE Tasks"
         self._execute_sql_command(sql_drop_table_command)
 
-
+    # Adds new record into table 'Tasks'. taskID is primary key. All parameters need to be formatted as sql values
     def Add_Record(self, taskID:str, taskEtag:str, taskTitle:str, taskStatus:str, timeUpdated:str, timeDue:str, timeCompleted:str):
         sql_add_record_command = "INSERT INTO Tasks VALUES ("
         sql_add_record_command += "'" + taskID + "', '" + taskEtag + "', '" + taskTitle + "', '" + taskStatus + "', '" + timeUpdated + "', "
@@ -64,7 +64,7 @@ class DatabaseClass:
             sql_add_record_command += "NULL);"
         self._execute_sql_command(sql_add_record_command)
 
-
+    # Updates an existing record identified via taskID. Leave a parameter as "" if it doesn't have to be changed
     def Update_Record(self, taskID:str, newtaskEtag:str, newtaskTitle:str, newtaskStatus:str, newtimeUpdated:str, newtimeDue:str, newTimeCompleted:str):
         sql_update_record_command = "UPDATE Tasks SET "
         if newtaskEtag != "":
@@ -82,18 +82,18 @@ class DatabaseClass:
         sql_update_record_command = sql_update_record_command[:-2] + " WHERE taskID = '" + taskID + "';"
         self._execute_sql_command(sql_update_record_command)
 
-
+    # Deletes a record identified via taskID
     def Delete_Record(self,taskID:int):
         sql_remove_record_command = "DELETE FROM Tasks WHERE taskID='" + taskID + "';"
         self._execute_sql_command(sql_remove_record_command)
 
-
+    # Returns a list of all records. Each record is a tuple
     def Get_Records_All(self):
         sql_get_records_all_command = "SELECT * FROM Tasks"
         records = self._cursor.execute(sql_get_records_all_command).fetchall()
         return records
 
-
+    # Returns a list of all records with the same taskStatus as provided. Each record is a tuple
     def Get_Records_Status(self, taskStatus:str):
         sql_get_Records_Status_command = "SELECT * FROM Tasks WHERE taskStatus='" + taskStatus +"';"
         records = self._cursor.execute(sql_get_Records_Status_command).fetchall()
